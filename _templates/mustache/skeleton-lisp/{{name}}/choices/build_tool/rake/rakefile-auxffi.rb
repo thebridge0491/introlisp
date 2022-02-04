e35edb4ffa8ad27efcdfd9e96fcd29498c8d51d9
thebridge0491 <thebridge0491-codelab@yahoo.com>
2022-02-04 15:50:57 +0000

    Update template - modify build tools ; update depns

--- _templates/mustache/skeleton-lisp/{{name}}/choices/build_tool/rake/rakefile-auxffi.rb
+++ _templates/mustache/skeleton-lisp/{{name}}/choices/build_tool/rake/rakefile-auxffi.rb
@@ -1,10 +1,11 @@
 # FFI auxiliary rakefile script
+
 FFI_LIBDIR = `#{PKG_CONFIG} --variable=libdir intro_c-practice || echo .`
 FFI_INCDIR = `#{PKG_CONFIG} --variable=includedir intro_c-practice || echo .`
 ENV['LD_LIBRARY_PATH'] = "#{ENV['LD_LIBRARY_PATH']}:#{FFI_LIBDIR}"
 sh "export LD_LIBRARY_PATH"
 
-#desc 'Prepare Swig files'
-#task :prep_swig do
-#  sh "swig -cffi -noswig-lisp -I#{FFI_INCDIR} -outdir build src/classic-c.i || true"
-#end
+desc 'Prepare Swig files'
+task :prep_swig do
+  sh "swig -cffi -noswig-lisp -I#{FFI_INCDIR} -outdir build src/classic-c.i || true"
+end
--- _templates/mustache/skeleton-lisp/{{name}}/choices/build_tool/rake/rakefile-targets.rb
+++ _templates/mustache/skeleton-lisp/{{name}}/choices/build_tool/rake/rakefile-targets.rb
@@ -28,48 +28,51 @@ end
 desc 'Uninstall artifacts'
 task :uninstall do 
   rm_rf("#{ENV["HOME"]}/quicklisp/local-projects/#{VARS.parent}/#{VARS.proj}") || true
-  sh "rlwrap #{COMPILER} --eval '(progn (ql:register-local-projects) (uiop:quit))' || true"
+  sh "rlwrap #{COMPILER} --eval '(progn (ql:register-local-projects) (format t \"~%~a~%\" (find (quote \"#{VARS.proj}\") (ql:list-local-systems) :test (quote equal))) (uiop:quit))' || true"
 end
 
 desc 'Install artifacts'
 task :install do
   ln_sf("#{ENV['PWD']}", "#{VARS.proj}") || true
   mv("#{VARS.proj}", "#{ENV["HOME"]}/quicklisp/local-projects/#{VARS.parent}/") || true
-  sh "rlwrap #{COMPILER} --eval '(progn (ql:register-local-projects) (uiop:quit))' || true"
+  sh "rlwrap #{COMPILER} --eval '(progn (ql:register-local-projects) (format t \"~%~a~%\" (find (quote \"#{VARS.proj}\") (ql:list-local-systems) :test (quote equal))) (uiop:quit))' || true"
 end
 
-file "build/#{VARS.name}-#{VARS.version}" do |p|
+file "build/#{VARS.proj}-#{VARS.version}" do |p|
   mkdir_p(p.name)
   # sh "zip -9 -q -x @exclude.lst -r - . | unzip -od #{p.name} -"
   sh "tar --posix -L -X exclude.lst -cf - . | tar -xpf - -C #{p.name}"
 end
 if defined? Rake::PackageTask
-  Rake::PackageTask.new(VARS.name, VARS.version) do |p|
-    # task("build/#{VARS.name}-#{VARS.version}").invoke
+  Rake::PackageTask.new(VARS.proj, VARS.version) do |p|
+    # task("build/#{VARS.proj}-#{VARS.version}").invoke
     
-    ENV.fetch('FMTS', 'tar.gz').split(',').each{|fmt|
+    ENV.fetch('FMTS', 'tar.gz,zip').split(',').each{|fmt|
       if p.respond_to? "need_#{fmt.tr('.', '_')}="
         p.send("need_#{fmt.tr('.', '_')}=", true)
       else
         p.need_tar_gz = true
       end
     }
-    task(:package).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz')}]"
-    task(:repackage).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz')}]"
+    task(:package).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz,zip')}]"
+    task(:repackage).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz,zip')}]"
   end
 else
-  desc "[FMTS=#{ENV.fetch('FMTS', 'tar.gz')}] Package project distribution"
-  task :dist => ["build/#{VARS.name}-#{VARS.version}"] do |t|
-    distdir = "#{VARS.name}-#{VARS.version}"
+  desc "[FMTS=#{ENV.fetch('FMTS', 'tar.gz,zip')}] Package project distribution"
+  task :package => ["#{VARS.proj}-#{VARS.version}"] do |t|
+    distdir = "#{VARS.proj}-#{VARS.version}"
     
-    ENV.fetch('FMTS', 'tar.gz').split(',').each{|fmt|
+    ENV.fetch('FMTS', 'tar.gz,zip').split(',').each{|fmt|
       case fmt
+      when '7z'
+        rm_rf("build/#{distdir}.7z") || true
+        cd('build') {sh "7za a -t7z -mx=9 #{distdir}.7z #{distdir}" || true}
       when 'zip'
         rm_rf("build/#{distdir}.zip") || true
         cd('build') {sh "zip -9 -q -r #{distdir}.zip #{distdir}" || true}
       else
-        # tarext = `echo #{fmt} | grep -e '^tar$' -e '^tar.xz$' -e '^tar.bz2$' || echo tar.gz`.chomp
-        tarext = fmt.match(%r{(^tar$|^tar.xz$|^tar.bz2$)}) ? fmt : 'tar.gz'
+        # tarext = `echo #{fmt} | grep -e '^tar$' -e '^tar.xz$' -e '^tar.zst$' -e '^tar.bz2$' || echo tar.gz`.chomp
+        tarext = fmt.match(%r{(^tar$|^tar.xz$|^tar.zst$|^tar.bz2$)}) ? fmt : 'tar.gz'
         rm_rf("build/#{distdir}.#{tarext}") || true
         cd('build') {sh "tar --posix -L -caf #{distdir}.#{tarext} #{distdir}" || true}
       end
